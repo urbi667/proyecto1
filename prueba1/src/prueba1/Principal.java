3693fea9ff1bd1cffd28293b18731f7d820c4c89
jortizdeurinam01 <jortizdeurinam01@iesalbarregas.es>
2021-03-01 18:20:49 +0100

    Validamos cambios, 3er commit

--- prueba1/src/prueba1/Principal.java
+++ prueba1/src/prueba1/Principal.java
@@ -6,6 +6,7 @@ public class Principal {
 		// TODO Auto-generated method stub
 		System.out.println("Hola Mundo java-git.");
 		System.out.println("Primer cambio realizado");
+		System.out.println("Segundo cambio realizado");
 	}
 
 }
